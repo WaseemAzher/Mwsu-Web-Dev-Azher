4a6e44394f6862ccf2de3ea9dc8b6c3f08133921
root <waseemazher89@gmail.com>
2016-07-15 12:25:21 -0400

    adding program_2 to github

---
+++ Program_2/error.php
@@ -0,0 +1,4 @@
+<?php
+print_r($_SESSION);
+
+echo"<h1> Email id or Password doesn't Match </h1>";
---
+++ Program_2/login.php
@@ -0,0 +1,188 @@
+<?php
+session_start();
+include_once 'dbconnect.php';
+
+if(isset($_SESSION['userSession'])!="")
+{
+    header("Location: home.php");
+    exit;
+}
+ 
+ //echo password_hash('1234', PASSWORD_DEFAULT);
+ //echo"<br>";
+ 
+if(isset($_POST['Signin']))
+{
+    $email = trim($_POST['email']);
+    $upass = trim($_POST['password']);
+    //echo"$email , $upass<br>";
+    
+    $query = $conn->query("SELECT * FROM users WHERE email='$email'");
+
+    $row=$query->fetch_array();
+    
+    if(password_verify($upass,$row['password'])){
+        header("Location: home.php");
+    }
+	
+	else 
+	{  
+		header("Location: error.php");
+	}
+ 
+  
+ $conn->close();
+}
+
+//  
+//  if(trim($hash) == md5(trim($row['password'])))
+//  {
+//   $_SESSION['userSession'] = $row['user_id'];
+//   echo"worked<br>";
+//   header("Location: home.php");
+//  }
+//  else
+//  {
+//   $msg = "
+//         email or password does not exists !
+//     ";
+//  }
+//  
+//  $conn->close();
+//  
+// }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset=utf-8 />
+	<title>Basic Web Page</title>
+	<link rel="stylesheet" type="text/css" media="screen" href="css/master.css" />
+	<link rel="stylesheet" type="text/css" media="screen" href="css/bootstrap.min.css" />
+	    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+	<script   src="https://code.jquery.com/jquery-3.1.0.min.js"   integrity="sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s="   crossorigin="anonymous"></script>
+	<!--[if IE]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	<style>
+		/* Credit to bootsnipp.com for the css for the color graph */
+		.colorgraph {
+		  height: 5px;
+		  border-top: 0;
+		  background: #c4e17f;
+		  border-radius: 5px;
+		  background-image: -webkit-linear-gradient(left, #c4e17f, #c4e17f 12.5%, #f7fdca 12.5%, #f7fdca 25%, #fecf71 25%, #fecf71 37.5%, #f0776c 37.5%, #f0776c 50%, #db9dbe 50%, #db9dbe 62.5%, #c49cde 62.5%, #c49cde 75%, #669ae1 75%, #669ae1 87.5%, #62c2e4 87.5%, #62c2e4);
+		  background-image: -moz-linear-gradient(left, #c4e17f, #c4e17f 12.5%, #f7fdca 12.5%, #f7fdca 25%, #fecf71 25%, #fecf71 37.5%, #f0776c 37.5%, #f0776c 50%, #db9dbe 50%, #db9dbe 62.5%, #c49cde 62.5%, #c49cde 75%, #669ae1 75%, #669ae1 87.5%, #62c2e4 87.5%, #62c2e4);
+		  background-image: -o-linear-gradient(left, #c4e17f, #c4e17f 12.5%, #f7fdca 12.5%, #f7fdca 25%, #fecf71 25%, #fecf71 37.5%, #f0776c 37.5%, #f0776c 50%, #db9dbe 50%, #db9dbe 62.5%, #c49cde 62.5%, #c49cde 75%, #669ae1 75%, #669ae1 87.5%, #62c2e4 87.5%, #62c2e4);
+		  background-image: linear-gradient(to right, #c4e17f, #c4e17f 12.5%, #f7fdca 12.5%, #f7fdca 25%, #fecf71 25%, #fecf71 37.5%, #f0776c 37.5%, #f0776c 50%, #db9dbe 50%, #db9dbe 62.5%, #c49cde 62.5%, #c49cde 75%, #669ae1 75%, #669ae1 87.5%, #62c2e4 87.5%, #62c2e4);
+		}
+	</style>
+</head>
+<body>
+<div class="container">
+
+<div class="row" style="margin-top:20px">
+    <div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
+		<form role="form" action="login.php" method="Post">
+		
+					<fieldset>
+				<h2>Please Sign In</h2>
+				<hr class="colorgraph">
+				<div class="form-group">
+                    <input type="email" name="email" id="email" class="form-control input-lg" placeholder="Email Address">
+				</div>
+				<div class="form-group">
+                    <input type="password" name="password" id="password" class="form-control input-lg" placeholder="Password">
+				</div>
+				<span class="button-checkbox">
+					<button type="button" class="btn" data-color="info">Remember Me</button>
+                    <input type="checkbox" name="remember_me" id="remember_me" checked="checked" class="hidden">
+					<a href="" class="btn btn-link pull-right">Forgot Password?</a>
+				</span>
+				<hr class="colorgraph">
+				<div class="row">
+					<div class="col-xs-6 col-sm-6 col-md-6">
+                        <input type="submit" class="btn btn-lg btn-success btn-block" value="Sign In" id="btnSignin" name="Signin">
+					</div>
+					<div class="col-xs-6 col-sm-6 col-md-6">
+						<a href="" class="btn btn-lg btn-primary btn-block">Register</a>
+					</div>
+				</div>
+			</fieldset>
+		</form>
+	</div>
+</div>
+
+</div>
+<script>
+$(function(){
+	/* 
+	$('#btnSignin').click(function(){
+		 		
+		           window.top.location.href = "login_validate.php"; 
+ 
+	}); */
+	
+	
+	
+    $('.button-checkbox').each(function(){
+        var $widget = $(this),
+            $button = $widget.find('button'),
+            $checkbox = $widget.find('input:checkbox'),
+            color = $button.data('color'),
+            settings = {
+                    on: {
+                        icon: 'glyphicon glyphicon-check'
+                    },
+                    off: {
+                        icon: 'glyphicon glyphicon-unchecked'
+                    }
+            };
+
+        $button.on('click', function () {
+            $checkbox.prop('checked', !$checkbox.is(':checked'));
+            $checkbox.triggerHandler('change');
+            updateDisplay();
+        });
+
+        $checkbox.on('change', function () {
+            updateDisplay();
+        });
+
+        function updateDisplay() {
+            var isChecked = $checkbox.is(':checked');
+            // Set the button's state
+            $button.data('state', (isChecked) ? "on" : "off");
+
+            // Set the button's icon
+            $button.find('.state-icon')
+                .removeClass()
+                .addClass('state-icon ' + settings[$button.data('state')].icon);
+
+            // Update the button's color
+            if (isChecked) {
+                $button
+                    .removeClass('btn-default')
+                    .addClass('btn-' + color + ' active');
+            }
+            else
+            {
+                $button
+                    .removeClass('btn-' + color + ' active')
+                    .addClass('btn-default');
+            }
+        }
+        function init() {
+            updateDisplay();
+            // Inject the icon if applicable
+            if ($button.find('.state-icon').length == 0) {
+                $button.prepend('<i class="state-icon ' + settings[$button.data('state')].icon + '"></i> ');
+            }
+        }
+        init();
+    });
+});
+</script>
+</body>
+</html>
